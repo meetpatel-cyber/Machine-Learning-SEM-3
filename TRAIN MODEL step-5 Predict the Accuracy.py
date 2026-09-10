cd5cdce55197fc0caa1e500ccb7f0276d30f34c1
Meet Patel <meetpatel4302@gmail.com>
2026-09-10 22:11:00 +0530

    Add train model step 5 & some changes

--- TRAIN MODEL step-5 Predict the Accuracy.py
+++ TRAIN MODEL step-5 Predict the Accuracy.py
@@ -1,10 +1,12 @@
-#STEP-4 TRAINING THE MODEL
+#STEP-5 Predict the Accuracy
 
 from sklearn.datasets import load_iris
 from sklearn.model_selection import train_test_split
-#########################################################
 from sklearn.linear_model import LogisticRegression
-#########################################################
+
+####################
+from sklearn import metrics
+####################
 
 iris = load_iris()
 
@@ -13,11 +15,14 @@ y = iris.target
 
 x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.4, random_state=1)
 
-#########################################################
-
 log_reg = LogisticRegression(max_iter=200)
 log_reg.fit(x_train, y_train)
 
 print("Model trained successfully")
+
+#########################################################
+
+y_predict = log_reg.predict(x_test)
+print(metrics.accuracy_score(y_test, y_predict))
 
 #########################################################
